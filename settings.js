6b7f454f98b8f890ba20d22c63dfd25ec5fc421f
Yasirulakshan960 <114982933+Yasirulakshan960@users.noreply.github.com>
2022-10-04 21:32:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,7 +37,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
 global.vcardowner = ['94779519998'] //ur owner number
-global.ownername = "yasiruuuu" //ur owner name
+global.ownername = "yasiya" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
@@ -46,15 +46,15 @@ global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 global.owner = ['94779519998']
 global.ownertag = '94779519998' //ur tag number
 global.botnumber = '94779519998' //Add your Bot Number For Log
-global.botname = 'yasiruuuu' //ur bot name
+global.botname = 'yasiya' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "yasiruuuu" //ur sticker watermark packname
-global.author = "yasiruuuu" //ur sticker watermark author
+global.packname = "yasiya" //ur sticker watermark packname
+global.author = "yasiya" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
